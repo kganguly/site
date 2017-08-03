f5270a1253a8022b021a5446c00f4cf2dd9951b5
Kaushik Ganguly <kganguly@gmail.com>
2017-08-02 21:24:54 -0400

    Added modal descriptions and links to the app or its code.

--- js/index.js
+++ js/index.js
@@ -27,6 +27,25 @@ function setListeners() {
     event.preventDefault();
     scrollToTop();
   });
+  $('#modal').on('show.bs.modal', function (event) {
+    var a = $(event.relatedTarget); // Button that triggered the modal
+    var index = a.data('index'); // Extract info from data-* attributes
+    var port = portfolioArray[index];
+    var modal = $(this);
+    modal.find('.modal-title').text(port.title);
+    var img = '<img src="' + port.img + '" alt="' + port.title + '" class="img-responsive" onload="thumbLoaded(this)">';
+    modal.find('.modal-body').html(port.desc + img);
+    $("#appButton").off();
+    $("#appButton").on("click", function () {
+      window.open(port.appUrl, "_blank");
+    });
+    $("#codeButton").off();
+    $("#codeButton").on("click", function () {
+      window.open(port.codeUrl, "_blank");
+    });
+  });
+
+
 }
 
 function buildPortfolio() {
@@ -36,7 +55,8 @@ function buildPortfolio() {
     var entry = portfolioArray[i];
     if (i % 2 === 0) portfolioHtml += '<div class="row text-center">';
     portfolioHtml += '<div class="col-sm-6 text-center">' +
-      '<a href="' + entry.href + '" target="_blank" class="thumbnail">' +
+      // '<a href="' + entry.href + '" target="_blank" class="thumbnail" data-toggle:"modal">' +
+      '<a href="#modal" class="thumbnail" data-toggle="modal" data-index="' + i + '">' +
       '<img src="' + entry.img + '" alt="' + entry.title + '" class="img-responsive" onload="thumbLoaded(this)">' +
       '</a>' +
       '</div>';
@@ -59,26 +79,44 @@ function scrollToElement(element) {
 
 portfolioArray = [{
   title: "Local Weather",
-  href: "https://codepen.io/kganguly/full/OWKYqL/",
-  img: "https://www.kganguly.com/img/LocalWeather_Thumb_Zoom.png"
+  appUrl: "https://codepen.io/kganguly/full/OWKYqL/",
+  codeUrl: "https://codepen.io/kganguly/pen/OWKYqL",
+  img: "https://www.kganguly.com/img/LocalWeather_Thumb_Zoom.png",
+  desc: "A Local Weather app using Google Maps and Weather Underground APIs.  An early exploration into composing with multiple APIs and responsive web design."
 }, {
   title: "Wikipedia Search",
-  href: "http://codepen.io/kganguly/full/ZeeEgr/",
-  img: "https://www.kganguly.com/img/WikiSearch_Thumb.png"
+  appUrl: "http://codepen.io/kganguly/full/ZeeEgr/",
+  codeUrl: "http://codepen.io/kganguly/pen/ZeeEgr/",
+  img: "https://www.kganguly.com/img/WikiSearch_Thumb.png",
+  desc: "A Wikipedia search interface with 'Load more...' options.  An Exploration into incremental loading and bootstrap with responsive web design."
 }, {
   title: "Twitch Stream Tracker",
-  href: "http://codepen.io/kganguly/full/QvLzpe/",
-  img: "https://www.kganguly.com/img/Twitch_Thumb.png"
-}, {
-  title: "Random Quote Generator",
-  href: "http://codepen.io/kganguly/full/dNegKb/",
-  img: "https://www.kganguly.com/img/RandomQuote_Thumb_Zoom.png"
-}, {
-  title: "Pomodoro Clock",
-  href: "https://kganguly.github.io/FCC-Pomodoro/",
-  img: "https://www.kganguly.com/img/Pomodoro_Thumb.png"
+  appUrl: "http://codepen.io/kganguly/full/QvLzpe/",
+  codeUrl: "http://codepen.io/kganguly/pen/QvLzpe/",
+  img: "https://www.kganguly.com/img/Twitch_Thumb.png",
+  desc: "An app that uses the Twitch API to get the current status of users.  I used this as an eploration into Promises and asynchronous coding.  While not necessary at this scale, it would scale well.  Responsive web design baked in."
 }, {
   title: "Calculator",
-  href: "https://kganguly.github.io/FCC-Calculator/",
-  img: "https://www.kganguly.com/img/Calculator_Thumb.png"
-}];;
+  appUrl: "https://kganguly.github.io/FCC-Calculator/",
+  codeUrl: "https://github.com/kganguly/FCC-Calculator",
+  img: "https://www.kganguly.com/img/Calculator_Thumb.png",
+  desc: "A Calculator with operator precedence.  This became an exploration into thorough unit testing with Jasmine and Karma."
+}, {
+  title: "Pomodoro Clock",
+  appUrl: "https://kganguly.github.io/FCC-Pomodoro/",
+  codeUrl: "https://github.com/kganguly/FCC-Pomodoro",
+  img: "https://www.kganguly.com/img/Pomodoro_Thumb.png",
+  desc: "This app is a configurable timer to schedule regular breaks.  This lead to exploring linear gradients and audio on mobile browsers.  Responsive web design baked in as usual."
+}, {
+  title: "Tic-Tac-Toe",
+  appUrl: "https://kganguly.github.io/FCC-Tic-Tac-Toe/",
+  codeUrl: "https://github.com/kganguly/FCC-Tic-Tac-Toe",
+  img: "https://www.kganguly.com/img/Tic-Tac-Toe_Thumb.png",
+  desc: "Tic-Tac-Toe against a simple computer opponent."
+}, {
+  title: "Random Quote Generator",
+  appUrl: "http://codepen.io/kganguly/full/dNegKb/",
+  codeUrl: "http://codepen.io/kganguly/pen/dNegKb/",
+  img: "https://www.kganguly.com/img/RandomQuote_Thumb_Zoom.png",
+  desc: "A simple random quote generator.  An early exploration into composing with APIs and responsive web design."
+}];
